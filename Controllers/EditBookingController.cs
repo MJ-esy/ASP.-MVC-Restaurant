f8478b7afc631f85d8b94a6b05a5fd72331ff002
MJ Eng <mj.eng.contact@gmail.com>
2025-10-12 23:12:44 +0200

    Create HttpGet Controllers

--- Controllers/EditBookingController.cs
+++ Controllers/EditBookingController.cs
@@ -1,12 +1,94 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.MVC.Mapping;
+using ASP.MVC.Services.BookingServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.MVC.Controllers
 {
-    public class EditBookingController : Controller
+  public class EditBookingController : Controller
+  {
+    private readonly IBookingServices _booking;
+    public EditBookingController(IBookingServices bookingServices)
     {
-        public IActionResult EditBookingIndex()
-        {
-            return View();
-        }
+      _booking = bookingServices;
     }
+    public IActionResult EditBookings()
+    {
+      return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AllBookings()
+    {
+      var bookings = await _booking.GetAllBookings();
+      if (!bookings.Any())
+      {
+        ViewData["ErrorMessage"] = "No bookings found.";
+        return View("EditBookings");
+      }
+      return View(bookings);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> BookingById(int BookingId)
+    {
+      try
+      {
+        var booking = await _booking.GetBookingById(BookingId);
+        if (booking == null)
+        {
+          ViewData["ErrorMessage"] = "Booking with ID not found!";
+          return View("EditBookings");
+        }
+        var bookingVM = booking.ToBookingVM();
+        return View(bookingVM);
+      }
+      catch (Exception ex)
+      {
+        ViewData["ErrorMessage"] = $"Error: {ex.Message}";
+        return View("EditBookings");
+      }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> BookingByDate(DateTime date)
+    {
+      try
+      {
+        var bookings = await _booking.BookingByDate(date);
+        if (!bookings.Any())
+        {
+          ViewData["ErrorMessage"] = "No bookings found.";
+          return View("EditBookings");
+        }
+
+        var bookingVMs = bookings.Select(booking => booking.ToBookingVM()).ToList();
+        return View(bookingVMs);
+      }
+      catch (Exception ex)
+      {
+        ViewData["ErrorMessage"] = $"Error: {ex.Message}";
+        return View("EditBookings");
+      }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> BookingToday()
+    {
+      try
+      {
+        var bookings = await _booking.BookingsToday();
+        if (!bookings.Any())
+        {
+          ViewData["ErrorMessage"] = "No bookings found.";
+          return View("EditBookings");
+        }
+        return View(bookings);
+      }
+      catch (Exception ex)
+      {
+        ViewData["ErrorMessage"] = $"Error: {ex.Message}";
+        return View("EditBookings");
+      }
+    }
+  }
 }
