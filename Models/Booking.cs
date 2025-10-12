a0702a2328583628e42793215c436238958dd28a
MJ Eng <mj.eng.contact@gmail.com>
2025-10-12 23:11:52 +0200

    Update model properties to Web API

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,17 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ASP.MVC.Models
+﻿namespace ASP.MVC.Models
 {
-    public class Booking
-    {
-        public int BookingId { get; set; }
-        public int UserIdFk { get; set; }
-        public int TableIdFk { get; set; }
-        public DateTime startDateTime { get; set; }
-        public DateTime EndDateTime { get; set; }
-        public int GuestNum { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public int Status { get; set; }
-    }
+  public class Booking
+  {
+    public int BookingId { get; set; }
+    public int UserIdFk { get; set; }
+    public int TableIdFk { get; set; }
+    public string Name { get; set; }
+    public int TableNum { get; set; }
+    public DateTime StartDateTime { get; set; }
+    public DateTime EndDateTime { get; set; }
+    public int GuestNum { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public int Status { get; set; }
+  }
 }
--- Models/Menu.cs
+++ Models/Menu.cs
@@ -1,16 +1,14 @@
 ﻿namespace ASP.MVC.Models
 {
-    public class Menu
-    {
-        public int DishId { get; set; }
-        public string DishName { get; set; }
-        public decimal Price { get; set; }
-        public string? Description { get; set; }
-        public bool IsPopular { get; set; }
-        public string Category { get; set; }
-        public string Allergen { get; set; }
-        public string? ImageUrl { get; set; }
-
-
-    }
+  public class Menu
+  {
+    public int DishId { get; set; }
+    public string DishName { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public bool IsPopular { get; set; }
+    public string Category { get; set; }
+    public string Allergen { get; set; }
+    public string? ImageUrl { get; set; }
+  }
 }
