dc4520cb8e7f2b4e6da2a4d0b820e7fd924806b2
MJ Eng <mj.eng.contact@gmail.com>
2025-10-12 23:11:22 +0200

    Create Booking VM and map to DTO

---
+++ Mapping/BookingMappings.cs
@@ -0,0 +1,28 @@
+﻿using ASP.MVC.DTOs;
+using ASP.MVC.Models.ViewModel;
+
+namespace ASP.MVC.Mapping
+{
+  public static class BookingMappings
+  {
+    public static BookingVM ToBookingVM(this BookingDTO bookingDTO)
+    {
+      return new BookingVM
+      {
+        BookingId = bookingDTO.BookingId,
+        StartDateTime = bookingDTO.StartDateTime,
+        EndDateTime = bookingDTO.EndDateTime,
+        GuestNum = bookingDTO.GuestNum,
+        Name = bookingDTO.User != null ? bookingDTO.User.Name : null,
+        Email = bookingDTO.User != null ? bookingDTO.User.Email : null,
+        Phone = bookingDTO.User != null ? bookingDTO.User.Phone : null,
+        TableId = bookingDTO.Table != null ? bookingDTO.Table.TableId : null,
+        TableNum = bookingDTO.Table != null ? bookingDTO.Table.TableNum : null,
+        TableCapacity = bookingDTO.Table != null ? bookingDTO.Table.Capacity : null,
+        Status = bookingDTO.Status,
+        CreatedAt = bookingDTO.CreatedAt,
+        UpdatedAt = bookingDTO.UpdatedAt
+      };
+    }
+  }
+}
---
+++ Models/ViewModel/BookingVM.cs
@@ -0,0 +1,35 @@
+﻿using ASP.MVC.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.MVC.Models.ViewModel
+{
+  public class BookingVM
+  {
+    public int BookingId { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy HH.mm}")]
+    public DateTime StartDateTime { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy HH.mm}")]
+    public DateTime EndDateTime { get; set; }
+
+    public int GuestNum { get; set; }
+    public int UserIdFk { get; set; }
+
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+
+    public int? TableId { get; set; }
+    public int? TableNum { get; set; }
+    public int? TableCapacity { get; set; }
+
+    public BookingStatus Status { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy HH.mm}")]
+    public DateTime CreatedAt { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy HH.mm}")]
+    public DateTime? UpdatedAt { get; set; }
+  }
+}
