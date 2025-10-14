f949bdafdde607b19bf559a95df5cee8f88c60f9
MJ Eng <mj.eng.contact@gmail.com>
2025-10-14 10:34:31 +0200

    Add EditTableController with EditTables action method
    A new `EditTableController` class was introduced in the
    `ASP.MVC.Controllers` namespace. This controller inherits
    from the `Controller` base class and includes an `EditTables`
    action method that returns a `View` result. The
    `Microsoft.AspNetCore.Mvc` namespace was also imported to
    support the new functionality.

---
+++ Controllers/EditTableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.MVC.Controllers
+{
+  public class EditTableController : Controller
+  {
+    public IActionResult EditTables()
+    {
+      return View();
+    }
+  }
+}
